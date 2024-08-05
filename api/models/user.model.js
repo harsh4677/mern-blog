dc0e2daa684adc16577938d14880b4166a40cb6f
harsh4677 <harshsantoshsalve@gmail.com>
2024-08-05 16:14:23 +0530

    created mongoose Schema

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    email: {
+        type:String,
+        require:true,
+        unique:true,
+    },
+    password: {
+        type:String,
+        require:true,
+    },
+
+},{timestamps: true}
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
