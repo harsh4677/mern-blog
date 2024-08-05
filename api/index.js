1751b08af7e5f5899f8892e238d0bb3f9d57d94f
harsh4677 <harshsantoshsalve@gmail.com>
2024-08-05 21:02:12 +0530

    created Test API route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/user.route.js');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-.then( ()=>{console.log('Mongodb is connected')})
-.catch((err)=>{console.log(err)});
+  .then(() => {
+    console.log('Mongodb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.use(express.json()); // To parse JSON bodies
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRoutes);
+
---
+++ api/routes/user.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {test}  = require('../controllers/user.controller.js'); 
+
+const router = express.Router();
+
+router.get('/test', test)
+
+module.exports = router; 
+
+
+
