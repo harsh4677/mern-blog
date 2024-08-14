6733df314c405580a90c3bcb305a24be5902e5f5
harsh4677 <harshsantoshsalve@gmail.com>
2024-08-14 17:24:15 +0530

    add Create a post api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/user.route.js');
-const authRoutes = require('./routes/auth.route.js');
-const cookieParser = require('cookie-parser');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDB is connected');
+    console.log('MongoDb is connected');
   })
   .catch((err) => {
     console.log(err);
@@ -18,26 +19,23 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
-// Middleware
-app.use(express.json()); // To parse JSON bodies
-app.use(cookieParser()); // To parse cookies
+app.use(express.json());
+app.use(cookieParser());
 
-// Routes
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
-// Error Handling Middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error'; // Fixed typo here
+  const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
-});
-
-// Start Server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
 });
