c73ab52e4ec36bb2ab57537b9583e68575dec94a
harsh4677 <harshsantoshsalve@gmail.com>
2024-08-14 12:03:00 +0530

    Added delete userAPI route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {test, updateUser}  = require('../controllers/user.controller.js'); 
+const {test, updateUser, deleteUser}  = require('../controllers/user.controller.js'); 
 const {verifyToken} = require('../utils/verifyUser.js')
 
 const router = express.Router();
 
 router.get('/test', test)
-router.put('/update/:userId', verifyToken, updateUser)
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 module.exports = router;
 
